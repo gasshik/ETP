c89cac621a09f48897426e7cd60bf09e5d6dee76
gasshik <gashik7656@gmail.com>
2026-03-23 21:02:53 +0200

    Add a player profile

--- players/views.py
+++ players/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Player
 
 def player_list(request):
     players = Player.objects.all().order_by('nickname')
     return render(request, 'players/player_list.html', {'players': players})
+
+def player_detail(request, pk):
+    player = get_object_or_404(Player, pk=pk)
+    return render(request, 'players/player_detail.html', {'player': player})
