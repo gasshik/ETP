3beebe52549472208e67d105f9f1d85e31bcb19e
gasshik <gashik7656@gmail.com>
2026-03-25 18:55:53 +0200

    Додав команди на профіль гравця і вивів коректний вік гравця замість дати народження

--- players/models.py
+++ players/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django_countries.fields import CountryField
+from datetime import date
 
 
 class Team(models.Model):
@@ -23,6 +24,13 @@ class Player(models.Model):
     date_of_birth = models.DateField(null=True, blank=True)
     flag = CountryField(blank_label="Оберіть країну", null=True, blank=True)
     country = models.CharField(max_length=100)
+    @property
+    def age(self):
+        today = date.today()
+        res = today.year - self.date_of_birth.year
+        if (today.month, today.day) < (self.date_of_birth.month, self.date_of_birth.day):
+            res -= 1
+        return res
 
     def __str__(self):
         return self.nickname
